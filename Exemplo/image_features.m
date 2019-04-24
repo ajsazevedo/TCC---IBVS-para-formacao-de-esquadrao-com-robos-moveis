33debc0f1239d90c7d7e2de6c13eb448acd5a97b
Antonio Azevedo <antonioazevedo.123@gmail.com>
2019-04-24 05:25:14 -0300

    Removendo codigo alheio
    Removendo codigo alheio

--- Exemplo/image_features.m
+++
@@ -1,2 +0,0 @@
-function fcn=image_features(fc)
-fcn=[fc(1,:)./fc(2,:); fc(3,:)./fc(2,:)];
--- Exemplo/project_on_x_y_plane.m
+++
@@ -1,4 +0,0 @@
-function fcxy=project_on_x_y_plane(fc)
-% s = [ x , y , theta]
-% fw n x 4 vector of feature points
-fcxy=[fc(1,:); fc(2,:)];
